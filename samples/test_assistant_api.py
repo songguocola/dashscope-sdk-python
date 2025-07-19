b8467cf0bbfae0b7d6244289b08a643124539d17
Kevin Lin <gucheng02061152@163.com>
2025-07-19 16:39:32 +0800

    feat:assistant_api: support more parameters

--- samples/test_assistant_api.py
+++ samples/test_assistant_api.py
@@ -34,16 +34,28 @@ assistant = Assistants.create(
                 }
             }
         }],
+        top_k=10,
+        top_p=0.5,
+        temperature=1.0,
+        max_tokens=2048
 )
-print('\n初始Assistant')
-print(assistant)
+print(f'\n初始Assistant: request_id: {assistant.request_id}')
+print(f'{assistant}\n')
 
-# 更新智能体：仅更新模型，Tools不变
-assistant = Assistants.update(assistant.id, model='qwen-plus')
-print('\n更新Model后')
-print(assistant)
+print(f'top_p: {assistant.top_p}, top_k: {assistant.top_k}, temperature: {assistant.temperature}, max_tokens: {assistant.max_tokens}, object: {assistant.object}')
 
-# 更新智能体: 清空Tools
-assistant = Assistants.update(assistant.id, tools=[])
-print('\n清空Tools后')
-print(assistant)
+# ==== test case 2: 新增和更新参数 =====
+assistant = Assistants.update(assistant.id, top_k=9, top_p=0.4, temperature=0.9, max_tokens=1024)
+print(f'\n更新参数: request_id: {assistant.request_id}')
+print(f'top_p: {assistant.top_p}, top_k: {assistant.top_k}, temperature: {assistant.temperature}, max_tokens: {assistant.max_tokens}, object: {assistant.object}')
+
+# ===== test case 1:  清空tools =====
+# # 更新智能体：仅更新模型，Tools不变
+# assistant = Assistants.update(assistant.id, model='qwen-plus')
+# print('\n更新Model后')
+# print(assistant)
+#
+# # 更新智能体: 清空Tools
+# assistant = Assistants.update(assistant.id, tools=[])
+# print('\n清空Tools后')
+# print(assistant)
