dd84a6d8e6068f609c8d3016096d8fa89db34f9f
kevin <lu384090@gmail.com>
2026-08-04 16:34:30 +0800

    Add deprecation warnings to the Assistants API

--- dashscope/assistants/__init__.py
+++ dashscope/assistants/__init__.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) Alibaba, Inc. and its affiliates.
 
+import warnings
+
 # yapf: disable
 
 from dashscope.assistants.assistant_types import (
@@ -17,3 +19,13 @@ __all__ = [
     'AssistantFile',
     'DeleteResponse',
 ]
+
+# Deprecation warning
+_DEPRECATION_MSG = (
+    "The Assistants API (dashscope.assistants) is deprecated and will be "
+    "removed in a future release. Please migrate to the Responses API. "
+    "See https://help.aliyun.com/zh/model-studio/"
+    "synchronous-call-api-reference for migration details."
+)
+
+warnings.warn(_DEPRECATION_MSG, category=DeprecationWarning, stacklevel=2)
